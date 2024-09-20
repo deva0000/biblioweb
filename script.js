887894d24891d266263f4cc1a9f63353dea051df
Kaleo <kaleoalves2012@gmail.com>
2024-09-20 13:49:30 -0300

    Css fixes + minor js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://fyygxgzlxnerquwrfpzy.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5eWd4Z3pseG5lcnF1d3JmcHp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2NzYwNTcsImV4cCI6MjA0MjI1MjA1N30.OAASk220-J3v9aY3XmhPKY4QmAT9vgfb1oOhhfOUkCI';
 const supabase = createClient(supabaseUrl, supabaseKey);
-const container = document.getElementById('entries-container');
+
 // Function to read entries from Supabase
 window.readEntries = async function() {
     let { data, error } = await supabase.from('entries').select('*');
@@ -14,6 +14,7 @@ window.readEntries = async function() {
     } else {
     console.log('Entries:('+typeof data+")", data);
     // Display the data on your page
+    let container = document.getElementById('entries-container');
     for (let entry of data) {
         // Create a new div element
         let div = document.createElement('div');
@@ -33,6 +34,9 @@ window.readEntries = async function() {
     
         // Append the div to the container
         container.appendChild(div);
+        if (container.children.length == 1){
+            div.style.marginTop = '0';
+        }
     }
 }
 }
